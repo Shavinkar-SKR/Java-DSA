5ba57ce2f4325eabf63cfb270cef3f049a17e35a
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-18 00:04:00 +0530

    Implementing Selection sort

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,13 +1,25 @@
 package sort;
 
+import java.util.ArrayList;
+
 public class SelectionSort {
 
     public static void main(String[] args) {
-        int b = 5;
-        mystery(b, b--, --b, ++b);
-    }
 
-    public static void mystery(int x, int y, int z, int a){
-        System.out.println(x + " " + y + " " + z + " " + a);
+        int[] nums = {91, 32, 92, 13, 73, 14};
+        int max=0, swap=0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for(int j=1; j< nums.length-1-i; j++){
+                if(nums[i]< nums[j]){
+                    max = nums[j];
+                }else{
+                    max = nums[i];
+                }
+            }
+            swap = nums[nums.length-1];
+            nums[nums.length-1] = max;
+        }
+
     }
 }
