170780fa4c2eb46708cfb02b4685b8b26b59bb8f
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-17 22:58:39 +0530

    Implementing Binary Search

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -4,9 +4,20 @@ public class BinarySearch {
 
     public static void usingForLoop(){
         int[] numbers = {11,17,18,45,50,71,95};
-        int searchValue = 50;
+        int searchValue = 17;
 
-        int first=0,last=0,mid=0;
+        int low=0, mid=(numbers.length)/2, high=numbers.length-1;
+
+        if(searchValue<numbers[mid]){
+            high=mid-1;
+        }else{
+            low=mid+1;
+        }
+
+
+        for (int i = 0; i < numbers.length; i++) {
+
+        }
 
 
     }
--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -1,4 +1,11 @@
 package sort;
 
 public class BubbleSort {
+
+    public static void main(String[] args) {
+
+        String[] arr = new String[5];
+
+        System.out.println(arr[0]);
+    }
 }
