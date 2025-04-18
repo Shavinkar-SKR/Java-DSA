531981bd3d26c344dd5180bf77140061c0004cbf
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-18 14:56:43 +0530

    Updated Selection sort

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -7,19 +7,30 @@ public class SelectionSort {
     public static void main(String[] args) {
 
         int[] nums = {91, 32, 92, 13, 73, 14};
-        int max=0, swap=0;
 
-        for (int i = 0; i < nums.length; i++) {
-            for(int j=1; j< nums.length-1-i; j++){
-                if(nums[i]< nums[j]){
-                    max = nums[j];
-                }else{
-                    max = nums[i];
+        printArray(nums);
+        for(int i = 0; i< nums.length; i++){
+            int max=0;
+            for(int j = 0; j < nums.length-i; j++){
+                if( nums[max] < nums[j]){
+                    max = j;
                 }
             }
-            swap = nums[nums.length-1];
-            nums[nums.length-1] = max;
-        }
 
+            int swap = nums[nums.length-1-i]; //14
+            nums[nums.length-1-i] = nums[max]; //92
+            nums[max] = swap; //14
+        }
+        printArray(nums);
+    }
+
+    public static void printArray(int[] arr){
+        System.out.print("[ ");
+        for(int num: arr){
+            System.out.print(num);
+            System.out.print(", ");
+        }
+        System.out.print("]");
+        System.out.println("\n");
     }
 }
