514c6f1c915cec0758ea18e830f4d49a005da678
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-18 19:56:40 +0530

    Implemented Bubble sort

--- src/sort/BubbleSort.java
+++ src/sort/BubbleSort.java
@@ -4,8 +4,30 @@ public class BubbleSort {
 
     public static void main(String[] args) {
 
-        String[] arr = new String[5];
+        int[] nums = {91, 32, 92, 13, 73, 14};
+        int swap=0;
+        printArray(nums);
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = 0; j < nums.length-1-i; j++){
+                if(nums[j]>nums[j+1]){
+                    swap = nums[j];
+                    nums[j] = nums[j+1];
+                    nums[j+1] = swap;
+                }
+                printArray(nums);
+            }
+            printArray(nums);
 
-        System.out.println(arr[0]);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        System.out.print("[");
+        for(int num: arr){
+            System.out.print(num);
+            System.out.print(", ");
+        }
+        System.out.print("]");
+        System.out.println("\n");
     }
 }
