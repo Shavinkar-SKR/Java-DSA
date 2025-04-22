513f9192c4802b933de7a28221d84cd63ef81939
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-22 12:46:32 +0530

    Updated binary search using recursion

--- src/recursion/BinarySearchUsingRecursion.java
+++ src/recursion/BinarySearchUsingRecursion.java
@@ -2,9 +2,9 @@ package recursion;
 
 public class BinarySearchUsingRecursion {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10};
+        int[] arr = {11,22,33,44,55,66,77,88,99,100};
         int left = 0;
-        int target = 9;
+        int target = 100;
         int right = arr.length-1;
 
         System.out.println(search(arr, target, left, right));
@@ -16,9 +16,9 @@ public class BinarySearchUsingRecursion {
             return -1;
         }
 
-        int index = left + (right - left)/2;
+        int index = (left+right)/2;
         if(arr[index] == target){
-            return target;
+            return index;
         }
 
         if(target<arr[index]){
