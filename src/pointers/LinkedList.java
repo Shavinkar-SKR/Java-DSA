f688c2049b7d28b8034cfdfeaaa9988d4e0692e2
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-23 20:02:22 +0530

    Updated the Linked list implementation

--- src/pointers/LinkedList.java
+++ src/pointers/LinkedList.java
@@ -2,15 +2,27 @@ package pointers;
 
 public class LinkedList {
 
-    private int data;
-    private LinkedList next;
-    private LinkedList previous;
+    private Node head;
+    private Node tail;
+    private int size;
 
-    public void setData(LinkedList newData){
-        this.next = newData;
+    public LinkedList(){
+        this.size = 0;
     }
 
-    public int getData(){
-        return this.data;
+    private class Node{
+
+        private Node next;
+        private int data;
+
+        public Node(int data){
+            this.data = data;
+        }
+
+        public Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+
     }
 }
