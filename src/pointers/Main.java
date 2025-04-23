8b13f44aba97beb1e1a07ea455bce3aa37db2ba5
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-23 20:19:20 +0530

    Created a main class to test
    Implemented Singly Linked List worked

---
+++ src/pointers/Main.java
@@ -0,0 +1,17 @@
+package pointers;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList list = new LinkedList();
+
+        list.insertFirst(5);
+        list.insertFirst(2);
+        list.insertFirst(6);
+        list.insertFirst(8);
+        list.insertFirst(1);
+
+        list.display();
+    }
+}
