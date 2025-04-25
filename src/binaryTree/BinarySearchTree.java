de05b72852901a5fa36d5d4d0d286c15607ae3dc
Shavinkar-SKR <shavinkarskr17@gmail.com>
2025-04-25 22:39:31 +0530

    Updated the BST
    Added two insert functions

--- src/binaryTree/BinarySearchTree.java
+++ src/binaryTree/BinarySearchTree.java
@@ -21,4 +21,25 @@ public class BinarySearchTree {
         }
     }
 
+    public void insert(int value){
+        root = insert(value, this.root);
+    }
+
+    private Node insert(int value, Node node){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+
+        if(value < node.value){
+            node.left = insert(value, node.left);
+        }
+
+        if(value > node.value){
+            node.right = insert(value, node.right);
+        }
+
+        return node;
+    }
+
 }
